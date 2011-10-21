14ed77a6ea4ad38accf2d93783b43bca3115b6d0
Bernhard Posselt <bernhard.posselt@gmx.at>
2011-10-21 21:00:22 +0200

    added forms.py

---
+++ infoscreen/infoscreen_screen/forms.py
@@ -0,0 +1,7 @@
+# Django imports
+from django import forms
+from django.contrib.auth.models import User
+from django.conf import settings
+
+# Project imports
+from infoscreen.infoscreen_screen.models import *
