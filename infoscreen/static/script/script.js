648f98064e81e6a17e39e3ee80fa72d8b27c3e4c
Bernhard Posselt <bernhard.posselt@gmx.at>
2011-10-22 15:41:03 +0200

    added color hover for checkout purposes

--- infoscreen/static/script/script.js
+++ infoscreen/static/script/script.js
@@ -2,5 +2,21 @@
  * Execute at start
  */
 $(document).ready(function() { 
-    // write your code in here
+    
+    // toggle classes on hover
+    $("#content").hover( function(){
+        $("#header").removeClass("peace");
+        $("#footer").removeClass("peace");
+        $("#header").addClass("alarm");
+        $("#footer").addClass("alarm");
+    });
+    
+    // toggle classes on hover
+    $("#header").hover( function(){
+        $("#header").removeClass("alarm");
+        $("#footer").removeClass("alarm");
+        $("#header").addClass("peace");
+        $("#footer").addClass("peace");
+    });
+    
 });
