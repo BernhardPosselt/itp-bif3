7ab474604591ae3654031d8e2b5a3f3793635a3b
Bernhard Posselt <bernhard.posselt@gmx.at>
2011-11-28 14:02:45 +0100

    added test templates

--- infoscreen/infoscreen_screen/urls.py
+++ infoscreen/infoscreen_screen/urls.py
@@ -1,11 +1,18 @@
 # django imports
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 # normal views
 urlpatterns = patterns('infoscreen.infoscreen_screen.views.views',
     url(r'^$', 'index', name='index'),
-    url(r'^bildschirm/links/$', 'bildschirm_links', name='bildschirm_links'),
-    url(r'^bildschirm/rechts/$', 'bildschirm_rechts', name='bildschirm_rechts'),
+    #url(r'^bildschirm/links/$', 'bildschirm_links', name='bildschirm_links'),
+    #url(r'^bildschirm/rechts/$', 'bildschirm_rechts', name='bildschirm_rechts'),
+    
+    # only for test purposes
+    url(r'^1/$', direct_to_template, {'template': 'test/einsatz_links.html'} ),
+    url(r'^2/$', direct_to_template, {'template': 'test/einsatz_rechts.html'} ),
+    url(r'^3/$', direct_to_template, {'template': 'test/frieden_links.html'} ),
+    url(r'^4/$', direct_to_template, {'template': 'test/frieden_rechts.html'} ),
 )
 
 # ajax views
