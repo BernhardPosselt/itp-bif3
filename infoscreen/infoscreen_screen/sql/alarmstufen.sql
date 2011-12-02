d49f338f40e4e24220b6f0bbdbbe3e6acf884025
Daniel Herzog <daniel@debian.(none)>
2011-12-02 13:08:17 +0100

    sql directory added

---
+++ infoscreen/infoscreen_screen/sql/alarmstufen.sql
@@ -0,0 +1,10 @@
+INSERT INTO infoscreen_screen_alarmstufen VALUES ('B1');
+INSERT INTO infoscreen_screen_alarmstufen VALUES ('B2');
+INSERT INTO infoscreen_screen_alarmstufen VALUES ('B3');
+INSERT INTO infoscreen_screen_alarmstufen VALUES ('B4');
+INSERT INTO infoscreen_screen_alarmstufen VALUES ('S1');
+INSERT INTO infoscreen_screen_alarmstufen VALUES ('S2');
+INSERT INTO infoscreen_screen_alarmstufen VALUES ('S3');
+INSERT INTO infoscreen_screen_alarmstufen VALUES ('T1');
+INSERT INTO infoscreen_screen_alarmstufen VALUES ('T2');
+INSERT INTO infoscreen_screen_alarmstufen VALUES ('T3');
