9fa4c641713df28a43f00ab59def92a8db4ff8db
Bernhard Posselt <bernhard.posselt@gmx.at>
2011-12-15 17:04:37 +0100

    merged

--- infoscreen/infoscreen_screen/views/ajax.py
+++ infoscreen/infoscreen_screen/views/ajax.py
@@ -1,5 +1,5 @@
 # System includes
-
+from time import mktime
 
 # Django includes
 
@@ -13,7 +13,41 @@ def update(request):
     """
     Doku
     """
-    ctx = {}
+    news = News.objects.all().order_by('-modifiziert')
+    einsatz = Einsaetze.objects.filter(abgeschlossen=False).order_by('-modifiziert')
+    dispo = Dispos.objects.all().order_by('-modifiziert')
+    fahrzeug = Fahrzeuge.objects.all().order_by('-modifiziert')
+    geraet = Geraete.objects.filter(reperatur=True).order_by('-modifiziert')
+    ausrueckordnung = Ausrueckordnungen.objects.all().order_by('-modifiziert')
+    
+    # get the nr of elements on each page and the last modified element
+    frieden_rechts_anzahl = [len(news), len(geraet), len(fahrzeug)]
+    frieden_rechts_last = [
+            int(mktime(news[:1].modifiziert.timetuple()),
+            int(mktime(geraet[:1].modifiziert.timetuple()),
+            int(mktime(fahrzeug[:1].modifiziert.timetuple()),
+        ].sort()[-1] 
+    einsatz_links_anzahl = [len(einsatz), len(ausrueckordnung), len(fahrzeug)]
+    einsatz_links_last = 
+        [
+            int(mktime(einsatz[:1].modifiziert.timetuple()),
+            int(mktime(ausrueckordnung[:1].modifiziert.timetuple()),
+            int(mktime(fahrzeug[:1].modifiziert.timetuple()),
+        ].sort()[-1] 
+    einsatz_rechts_anzahl = [len(dispo), len(einsatz)]
+    einsatz_rechts_last = [
+            int(mktime(dispo[:1].modifiziert.timetuple()),
+            int(mktime(einsatz[:1].modifiziert.timetuple())
+        ].sort()[-1] 
+    ctx = {
+        'frieden_rechts_last': frieden_rechts_last,
+        'einsatz_links_last': einsatz_links_last,
+        'einsatz_rechts_last': einsatz_rechts_last,
+        # nr of elems
+        'frieden_rechts_anzahl': frieden_rechts_anzahl,
+        'einsatz_links_anzahl': einsatz_links_anzahl,
+        'einsatz_rechts_anzahl': einsatz_rechts_anzahl
+    }
     return render(request, "infoscreen_screen/ajax/update.json", ctx)
 
     
