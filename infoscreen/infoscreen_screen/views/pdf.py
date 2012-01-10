0e00a27788d981755b7f918d98ef2548ec2e4500
Markus Gruber <markusgruber10@hotmail.com>
2012-01-10 14:14:51 +0100

    pdf updated

--- infoscreen/infoscreen_screen/views/pdf.py
+++ infoscreen/infoscreen_screen/views/pdf.py
@@ -5,8 +5,10 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import cm
 from reportlab.lib.pagesizes import A4
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.styles import ParagraphStyle
 from reportlab.platypus import Frame, Paragraph
+from reportlab.lib.enums import TA_LEFT
+from reportlab.lib.colors import HexColor
 
 # Django includes
 from django.http import HttpResponse
@@ -40,7 +42,7 @@ def einsatzfax_pdf(request, id):
         einsatz = Einsaetze.objects.get(id=id)
         # Create the HttpResponse object with the appropriate PDF headers.
         response = HttpResponse(mimetype='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename=einsatzfax.pdf'
+        response['Content-Disposition'] = 'attachment; filename=einsatzfax.pdf' 
 
         # Create the PDF object, using the response object as its "file" in DIN A4 format
         p = canvas.Canvas(response,pagesize=A4)
@@ -48,7 +50,9 @@ def einsatzfax_pdf(request, id):
 	# A4 width and height
 	breite, hoehe = A4
 	# Define the styles
-	style = getSampleStyleSheet()
+	styleHeader = ParagraphStyle('Header', fontName='Helvetica', fontSize=36, spaceAfter=40, alignment=TA_LEFT)
+	styleText = ParagraphStyle('Text', fontName='Helvetica', fontSize=16, leading=20, alignment=TA_LEFT)
+	styleLine = ParagraphStyle('Line', fontName='Helvetica', fontSize=16, leading=20, alignment=TA_LEFT, textColor=HexColor(0xFFFFFF))
 	# Creating a list, which contains the content
 	content = []
 	# Create the frame where the content will be placed
@@ -60,18 +64,20 @@ def einsatzfax_pdf(request, id):
 	# Generate content
         meldeb = Meldebilder.objects.get(id = einsatz.meldebild_id)
         alarm = Alarmstufen.objects.get(stufe = meldeb.stufe_id)
-	content.append(Paragraph(alarm.stufe + " " + meldeb.beschreibung, style['Heading1']))
-	content.append(Paragraph(einsatz.strasse + " " + einsatz.nummer1 + " " + einsatz.nummer2 + " " + einsatz.nummer3, style['BodyText']))
-	content.append(Paragraph(str(einsatz.plz) + " " + einsatz.ort, style['BodyText']))
+	content.append(Paragraph(alarm.stufe + " " + meldeb.beschreibung, styleHeader))
+	content.append(Paragraph(einsatz.strasse + " " + einsatz.nummer1 + " " + einsatz.nummer2 + " " + einsatz.nummer3, styleText))
+	content.append(Paragraph(str(einsatz.plz) + " " + einsatz.ort, styleText))
+	content.append(Paragraph("a", styleLine))
 	if einsatz.objekt:
-		content.append(Paragraph("Objekt: " + einsatz.objekt, style['BodyText']))
+		content.append(Paragraph("Objekt: " + einsatz.objekt, styleText))
 	if einsatz.melder:
-		content.append(Paragraph("Melder: " + einsatz.melder, style['BodyText']))
+		content.append(Paragraph("Melder: " + einsatz.melder, styleText))
 	if einsatz.meldertel:
-		content.append(Paragraph("Meldertelefon: " + einsatz.meldertel, style['BodyText']))
+		content.append(Paragraph("Meldertelefon: " + einsatz.meldertel, styleText))
 	if einsatz.einsatzerzeugt:
-		content.append(Paragraph("Meldezeitpunkt: " + str(einsatz.einsatzerzeugt), style['BodyText']))
-	content.append(Paragraph(einsatz.bemerkung, style['BodyText']))
+		content.append(Paragraph("Meldezeitpunkt: " + str(einsatz.einsatzerzeugt), styleText))
+	content.append(Paragraph("a", styleLine))
+	content.append(Paragraph(einsatz.bemerkung, styleText))
 
 	# Add content
 	f.addFromList(content,p)
