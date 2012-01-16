828e7afa9f42d51a6a7a9d18f956a2cdb2c2165b
Bernhard Posselt <bernhard.posselt@gmx.at>
2012-01-16 21:47:19 +0100

    minified deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# software needed to build: 
-apt-get -y install dpkg-dev debhelper build-essential dh-make
-
 # purge and install infoscreen
 apt-get -y purge infoscreen
-git pull
-./build
 dpkg -i infoscreen*.deb
 apt-get -y -f install
 
