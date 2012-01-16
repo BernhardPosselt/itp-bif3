8e9635deb25b54507070ee01ee20e6952cd749d4
Bernhard Posselt <bernhard.posselt@gmx.at>
2012-01-16 21:27:07 +0100

    removed old tutorials

--- doc/tutorials/daniel/blog/admin.py
+++
@@ -1,7 +0,0 @@
-from blog.models import *
-from django.contrib import admin
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('titel', 'date')   
-    
-admin.site.register(Post, PostAdmin)
